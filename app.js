c1536e2b2a951371aa58fd3d5bc54fcfbeb04e76
GeoSauer <geoffrey.sauer89@gmail.com>
2022-09-13 19:37:55 -0600

    commented out code to pass eslint

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /* Imports */
-import { getRandomItem } from './utils.js';
+// import { getRandomItem } from './utils.js';
 
 /* State */
-let gameState = 'guess'; // 'guess' or 'results'
-let guess = ''; // 'left' 'middle' or 'right'
-let pearl = ''; // 'true' or 'false'
-let result = ''; //'win' or 'lose'
+// let gameState = 'guess'; // 'guess' or 'results'
+// let guess = ''; // 'left' 'middle' or 'right'
+// let pearl = ''; // 'true' or 'false'
+// let result = ''; //'win' or 'lose'
 
 /* Actions */
 function loadPage() {
@@ -13,13 +13,13 @@ function loadPage() {
     // displayResults();
 }
 
-const choices = ['left', 'middle', 'right'];
+// const choices = ['left', 'middle', 'right'];
 
-function findPearl(userGuess) {
-    gameState = 'results';
-    guess = userGuess;
-    pearl = getRandomItem(choices);
-}
+// function findPearl(userGuess) {
+//     gameState = 'results';
+//     guess = userGuess;
+//     pearl = getRandomItem(choices);
+// }
 
 /* Components */
 
@@ -29,13 +29,13 @@ const guess1 = document.getElementById('guess-1');
 const guess2 = document.getElementById('guess-2');
 const guess3 = document.getElementById('guess-3');
 const shell1 = document.getElementById('shell-1');
-const shell2 = document.getElementById('shell-2');
-const shell3 = document.getElementById('shell-3');
+// const shell2 = document.getElementById('shell-2');
+// const shell3 = document.getElementById('shell-3');
 const pearl1 = document.getElementById('pearl-1');
-const pearl2 = document.getElementById('pearl-2');
-const pearl3 = document.getElementById('pearl-3');
+// const pearl2 = document.getElementById('pearl-2');
+// const pearl3 = document.getElementById('pearl-3');
 const display1 = document.getElementById('display-1');
-const display2 = document.getElementById('display-2');
+// const display2 = document.getElementById('display-2');
 const display3 = document.getElementById('display-3');
 const playAgain = document.getElementById('play-again-button');
 
@@ -57,27 +57,27 @@ function displayShells() {
     // }
 }
 
-function displayResults() {
-    display1.classList.remove('hidden');
-    display3.classList.remove('hidden');
-    playAgain.classList.remove('hidden');
-    guess1.classList.add('hidden');
-    guess2.classList.add('hidden');
-    guess3.classList.add('hidden');
-}
+// function displayResults() {
+//     display1.classList.remove('hidden');
+//     display3.classList.remove('hidden');
+//     playAgain.classList.remove('hidden');
+//     guess1.classList.add('hidden');
+//     guess2.classList.add('hidden');
+//     guess3.classList.add('hidden');
+// }
 
 // event listeners
 guess1.addEventListener('click', () => {
     // shell1.classList.add('reveal');
-    findPearl('left');
+    // findPearl('left');
 });
 guess2.addEventListener('click', () => {
     // shell2.classList.add('reveal');
-    findPearl('middle');
+    // findPearl('middle');
 });
 guess3.addEventListener('click', () => {
     // shell3.classList.add('reveal');
-    findPearl('right');
+    // findPearl('right');
 });
 //* Run page load code
 loadPage();
